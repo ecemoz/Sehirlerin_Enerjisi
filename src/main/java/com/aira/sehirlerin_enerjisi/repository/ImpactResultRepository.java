0895e2e804d32d2bc353a3fb53d21e1af586e7e8
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-07-05 22:30:00 +0300

    feat: Add ImpactResultRepository for managing ImpactResult entity operations

---
+++ src/main/java/com/aira/sehirlerin_enerjisi/repository/ImpactResultRepository.java
@@ -0,0 +1,7 @@
+package com.aira.sehirlerin_enerjisi.repository;
+
+import com.aira.sehirlerin_enerjisi.model.ImpactResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImpactResultRepository extends JpaRepository<ImpactResult, Integer> {
+}
