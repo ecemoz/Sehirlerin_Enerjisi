4be3cf29ab8c50c9bee04a26294654c4e0a2b6bf
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-07-05 22:34:12 +0300

    feat: Add ImpactController to expose impact calculation endpoint

---
+++ src/main/java/com/aira/sehirlerin_enerjisi/controller/ImpactController.java
@@ -0,0 +1,22 @@
+package com.aira.sehirlerin_enerjisi.controller;
+
+import com.aira.sehirlerin_enerjisi.dto.ImpactRequestDto;
+import com.aira.sehirlerin_enerjisi.dto.ImpactResponseDto;
+import com.aira.sehirlerin_enerjisi.service.ImpactService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/impact")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+public class ImpactController {
+
+    private final ImpactService impactService;
+
+    @PostMapping
+    public List<ImpactResponseDto> calculateImpact(@RequestBody ImpactRequestDto request) {
+        return impactService.calculateImpact(request);
+    }
+}
