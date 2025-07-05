b9fd762b2b05ebace5113ed85585d203db8a814d
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-07-05 23:05:44 +0300

    feat: ImpactRequestDto is updated.

--- src/main/java/com/aira/sehirlerin_enerjisi/dto/ImpactRequestDto.java
+++ src/main/java/com/aira/sehirlerin_enerjisi/dto/ImpactRequestDto.java
@@ -1,9 +1,18 @@
 package com.aira.sehirlerin_enerjisi.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class ImpactRequestDto {
-    private double latitude;
-    private double longitude;
+
+    @NotNull(message = "Latitude boş olamaz")
+    @Min(value = -90, message = "Latitude -90'dan küçük olamaz")
+    @Max(value = 90, message = "Latitude 90'dan büyük olamaz")
+    private Double latitude;
+
+    @NotNull(message = "Longitude boş olamaz")
+    @Min(value = -180, message = "Longitude -180'den küçük olamaz")
+    @Max(value = 180, message = "Longitude 180'den büyük olamaz")
+    private Double longitude;
 }
