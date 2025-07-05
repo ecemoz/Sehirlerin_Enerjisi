f65a92044f4a918890cfaeba44c04eed872db7a5
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-07-06 00:10:46 +0300

    final project structure

--- src/main/java/com/aira/sehirlerin_enerjisi/controller/ImpactController.java
+++ src/main/java/com/aira/sehirlerin_enerjisi/controller/ImpactController.java
@@ -4,20 +4,23 @@ import com.aira.sehirlerin_enerjisi.dto.ImpactRequestDto;
 import com.aira.sehirlerin_enerjisi.dto.ImpactResponseDto;
 import com.aira.sehirlerin_enerjisi.service.ImpactService;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/impact")
-@RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 public class ImpactController {
 
     private final ImpactService impactService;
 
+    public ImpactController(ImpactService impactService) {
+        this.impactService = impactService;
+    }
+
     @PostMapping
-    public List<ImpactResponseDto> calculateImpact( @Valid @RequestBody ImpactRequestDto request) {
+    public List<ImpactResponseDto> calculateImpact(@Valid @RequestBody ImpactRequestDto request) {
         return impactService.calculateImpact(request);
     }
 }
+
