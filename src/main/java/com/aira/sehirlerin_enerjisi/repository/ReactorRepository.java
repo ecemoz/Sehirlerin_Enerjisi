8f32b607e101b71403c8787c927d23e0d5493ea3
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-07-05 22:27:57 +0300

    feat: Create ReactorRepository for managing Reactor entity persistence

---
+++ src/main/java/com/aira/sehirlerin_enerjisi/repository/ReactorRepository.java
@@ -0,0 +1,7 @@
+package com.aira.sehirlerin_enerjisi.repository;
+
+import com.aira.sehirlerin_enerjisi.model.Reactor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReactorRepository extends JpaRepository<Reactor, Integer> {
+}
